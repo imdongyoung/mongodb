1b360d21b794d00e3fa293c301cf48b7dd8a4f24
imdongyoung <imdong207@gmail.com>
2021-11-14 21:33:20 +0900

    nodejs교과서-mongodb

---
+++ schemas/index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const connect = () => {
+  if (process.env.NODE_ENV !== "production") {
+    mongoose.set("debug", true);
+  }
+  mongoose.connect("mongodb://imdong:816207@localhost:27017/admin", (error) => {
+    if (error) {
+      console.log("몽고디비 연결 에러", error);
+    } else {
+      console.log("몽고디비 연결 성골");
+    }
+  });
+};
+mongoose.connection.on("error", (error) => {
+  console.error("몽고디비 연결 에러", error);
+});
+mongoose.connection.on("disconnected", () => {
+  console.error("몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.");
+  connect();
+});
+
+module.exports = connect;
